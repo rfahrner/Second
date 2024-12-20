11e533277247ee643b8145029f97e6196a43f474
rfahrne <rfahrner@wgu.edu>
2024-12-20 17:03:28 -0600

    first atempt at javascript

--- website/validate.js
+++ website/validate.js
@@ -1,3 +1,7 @@
-if (verifyEmail != email) {
-    
+function emailVerify() {
+    let one = document.getElementById('verifyEmail')
+    let two = document.getElementById('email')
+    if (one == two) {return true}
+        else
+            return alert("Emails do not match")
 }
