e096749e9eecdfaaa43fbb5bbb1b56e62f518bb8
rfahrne <rfahrner@wgu.edu>
2024-12-21 10:29:42 -0600

    javascript fix

--- website/validate.js
+++ website/validate.js
@@ -1,12 +1,14 @@
 let form = document.getElementById('userForm')
 
 function emailVerify(event) {
-    let one = document.verifyEmail.value
-    let two = document.email.value
-    if (one == two) {return true}
-        else
-            return console.log("Emails entered do not match")
-            event.preventDefault();
-
+    let one = document.getElementById('verifyEmail').value;
+    let two = document.getElementById('email').value; 
+    if (one === two) {
+        return true;
+    } else {
+        alert("Emails entered do not match");
+        event.preventDefault();
+        return false;
+    }
 }
 form.addEventListener("submit",emailVerify)
